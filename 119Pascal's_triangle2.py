f0fe530efe6d35e497e0319af076d6b1bae0b27b
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-06-19 23:49:59 -0400

    Update 119Pascal's_triangle2.py

--- 119Pascal's_triangle2.py
+++ 119Pascal's_triangle2.py
@@ -9,3 +9,15 @@ class Solution(object):
         for i in range(1, rowIndex+1):
             res = [1] + [res[j] + res[j+1] for j in range(len(res)-1)] +[1]
         return res if rowIndex >=0 else None
+#method2
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        tmplist, m = [1], 1
+        for i in range(1, rowIndex+1):
+            m *= i
+            tmplist.append(m)
+        return [(tmplist[rowIndex]/tmplist[j]/tmplist[rowIndex-j]) for j in range(0, rowIndex +1)]

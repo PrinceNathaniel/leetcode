751e7b7cbb747dbbf977a69787f4cabb58cd157c
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-06-20 13:04:33 -0400

    Create 122Bust_time_to_buy_stocks2.py

---
+++ 122Bust_time_to_buy_stocks2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(prices)-1):
+            res += max(prices[i+1]-prices[i], 0)
+        return res

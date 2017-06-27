9e276c99a17ac549628ded954ae74885d6a9d11d
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-06-27 15:51:21 -0400

    Update 039Combination-sum.py

--- 039Combination-sum.py
+++ 039Combination-sum.py
@@ -22,3 +22,24 @@ class Solution(object):
         else:
             for index, number in enumerate(candidates):
                 self.combination(candidates[index:], target, tmp +[number], res)
+#Method 2:
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        res = []
+        self.dfs(res, candidates,0,target,[])
+        return res
+        
+    def dfs(self, res, candidates, tmp, target, l):
+        if target == 0 :
+            res.append(l)
+            return
+        for i in range(tmp, len(candidates)):
+            if candidates[i] > target:
+                return
+            self.dfs(res, candidates, i, target-candidates[i], l +[candidates[i]])

7aadec4bdb44ad08b792b1557dee7addbd0d3b87
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-08-03 12:47:52 -0400

    Create 078Subsets.py

---
+++ 078Subsets.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        for i in nums:
+            res += [j+[i] for j in res]
+        return res
+        

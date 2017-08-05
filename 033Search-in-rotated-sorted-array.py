aeefefca4801376ac024482b2823018608475169
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-08-05 15:53:53 -0400

    Update 033Search-in-rotated-sorted-array.py

--- 033Search-in-rotated-sorted-array.py
+++ 033Search-in-rotated-sorted-array.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right)/2
+            if nums[mid] == target:
+                return mid
+            elif (nums[mid] >= nums[left] and nums[left] <= target<nums[mid] )or (nums[mid]< nums[left] and not nums[mid]< target <=nums[right]):
+                right = mid-1
+            else:
+                left = mid +1
 
+        return -1

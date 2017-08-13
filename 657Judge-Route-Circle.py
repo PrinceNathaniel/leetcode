b204be72c755e92fc9513de75bb46d2541dd5914
PrinceNathaniel <nathanielxuyc@gmail.com>
2017-08-12 23:11:07 -0400

    Update 657Judge-Route-Circle.py

--- 657Judge-Route-Circle.py
+++ 657Judge-Route-Circle.py
@@ -1,1 +1,20 @@
-Judge Route Circle
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        u,d,l,r = 0,0,0,0
+        for c in moves:
+            if c=='U':
+                u+=1
+            elif c =='D':
+                d+=1
+            elif c=='L':
+                l+=1
+            else:
+                r+=1
+        if u==d and l==r:
+            return True
+        else:
+            return False
